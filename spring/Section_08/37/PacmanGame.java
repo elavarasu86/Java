a56e437bd9b5a5c7f4f7711d401fba7fa8fae4f7
elavarasu86 <94145348+elavarasu86@users.noreply.github.com>
2025-03-02 14:11:39 -0500

    Add files via upload

---
+++ spring/Section_08/37/PacmanGame.java
@@ -0,0 +1,20 @@
+package com.elavarasu.learn_spring_framework.game;
+
+public class PacmanGame implements GamingConsole {
+	public void up() {
+		System.out.println("Up");
+	}
+
+	public void down() {
+		System.out.println("Down");
+	}
+
+	public void right() {
+		System.out.println("Right");
+	}
+
+	public void left() {
+		System.out.println("Left");
+	}
+
+}
